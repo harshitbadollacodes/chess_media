18f052c4789245bb24ea49dde4d30b1690799fd0
harshitbadolla <harshitbadolla@yahoo.com>
2022-03-02 19:18:02 +0530

    added edit post & delete post button

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -1,17 +1,18 @@
 import { useEffect } from "react";
-import { FaBookmark, FaHeart, FaRegBookmark, FaRegHeart } from "react-icons/fa"
-import { BsTrash } from "react-icons/bs";
+import { FaBookmark, FaHeart, FaRegBookmark, FaRegComment, FaRegHeart } from "react-icons/fa"
+import { BsPencil, BsTrash } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Loader } from "../../components/Loader";
 import { likeButtonClicked, loadPostsData, removePost, savePost } from "./postSlice";
 
-export function PostsList({ posts }) {
+export function PostList({ posts }) {
 
     const { userId, token } = useSelector(state => state.user);
     const { status } = useSelector(state => state.posts);
     
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (status === "idle") {
@@ -53,12 +54,20 @@ export function PostsList({ posts }) {
                         <p className="capitalize"> { post.user.firstName } { post.user.lastName } </p>
                         <p className="font-extralight"> @{ post.user.username } </p>
                     </Link>   
-                    {
-                        post.user._id === userId &&
-                        <button onClick={() => dispatch(removePost({token, postId: post._id}))}>
-                            <BsTrash />
-                        </button>
-                    }
+                    
+                        {
+                            post.user._id === userId &&
+                            <div>
+                                <button onClick={() => dispatch(removePost({token, postId: post._id}))}>
+                                    <BsTrash />
+                                </button>
+
+                                <Link to={`/editPost/${post._id}`}>
+                                    <BsPencil />
+                                </Link>
+
+                            </div>
+                        }
                 </div>
 
                 <p className="text-xl my-2 w-full "> {post.postContent} </p>
@@ -82,6 +91,13 @@ export function PostsList({ posts }) {
                         
                         <p className="text-xl ml-2">{post.likes.length}</p>
                     </div>
+                    
+                    <div className="flex items-center w-fit">
+                        <button onClick={() => navigate(`/post/${post._id}`)}>
+                            <FaRegComment/> 
+                        </button>
+                        <p className="text-xl ml-2">{post.comments.length}</p>
+                    </div>
 
                     <button 
                         onClick={() => dispatch(savePost({token, postId: post._id}))}
@@ -92,10 +108,8 @@ export function PostsList({ posts }) {
                             : <FaRegBookmark/>
                         }
                     </button>
-
+                    
                 </div>
-
-
             </div>
         </li>
     ));

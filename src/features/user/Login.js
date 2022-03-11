c232e8b52313f9fffc4948fe8ddc393b266c27ba
harshitbadolla <harshitbadolla@yahoo.com>
2022-03-11 21:54:00 +0530

    Design update in login page

--- src/features/user/Login.js
+++ src/features/user/Login.js
@@ -1,14 +1,13 @@
 import chessBanner from "../../images/chessBanner.jpeg";
 import { LoginForm } from "./LoginForm";
 
-
 export function Login(){
 
     return (
-        <div className="flex flex-col lg:flex-row h-[90vh]">
+        <div className="flex flex-col w-full lg:flex-row h-[90vh] items-center">
             <div className="w-full lg:w-1/2">
                 <img 
-                    className="w-full lg:h-screen"
+                    className="w-full lg:rounded-xl"
                     src={chessBanner} 
                     alt="knight" 
                 />

a9f36a51e704226ba522f8d7082e0182d6512b75
harshitbadolla <harshitbadolla@yahoo.com>
2022-03-11 21:52:21 +0530

    Feat: Edit display picture

--- src/features/profile/EditBio.js
+++ src/features/profile/EditBio.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
@@ -9,7 +10,7 @@ export function EditBio() {
     const { userDetails, status } = useSelector(state => state.profile);
     
     const [inputBio, setInputBio] = useState(userDetails.bio);
-    console.log(inputBio);
+    const [displayPicture, setDisplayPicture] = useState("");
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
@@ -17,12 +18,27 @@ export function EditBio() {
     function editHandler() {
         dispatch(editBio({
             bio: inputBio, 
+            imageURL: displayPicture,
             token
         }));
     };
 
     function discardHandler() {
         navigate(`/profile/${userId}`);
+    };
+
+    async function uploadImageHandler(e) {
+        let image = e.target.files[0];
+
+        const formData = new FormData();
+        formData.append("file", image);
+        formData.append("upload_preset", "kutumlde");
+
+        const {status, data: {url} } = await axios.post("https://api.cloudinary.com/v1_1/dwrcvgzi0/image/upload", formData);
+
+        if (status === 200) {
+            setDisplayPicture(url);
+        };
     };
 
     useEffect(() => {
@@ -33,6 +49,23 @@ export function EditBio() {
 
     return (
         <div className="py-2 mr-2 mt-2 w-full lg:w-[60%]">
+
+            <div className="flex items-center my-4">
+                <img 
+                    src={displayPicture || userDetails.displayPicture} 
+                    className="h-10 w-10 md:h-20 md:w-20 border-2 rounded-full"
+                    alt={userDetails.firstName}
+                />
+
+                <form className="ml-2">
+                    <input 
+                        type="file" 
+                        className="custom-file-input my-2 cursor-pointer rounded-lg border border-d-blue p-2"
+                        onChange={(e) => uploadImageHandler(e)} 
+                    />
+                </form>
+            </div>
+
             <textarea 
                     onChange={(e) => setInputBio(e.target.value)}
                     value={inputBio}

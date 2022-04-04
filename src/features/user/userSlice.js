b975eb9c4e4f7f6ca680b2180f538ced7c1ea794
harshitbadolla <harshitbadolla@yahoo.com>
2022-04-04 15:23:33 +0530

    added savedPosts to intialState

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -69,7 +69,8 @@ export const userSlice = createSlice({
         username,
         token,
         error: null,
-        allUsers: []
+        allUsers: [],
+        savedPosts: []
     },
 
     reducers: {
